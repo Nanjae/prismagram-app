b7498ac20b667d10de9b005749563b23583fb58d
Nanjae <dngngn3045@gmail.com>
2019-11-08 00:44:27 +0900

    #11.3 Photo Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,24 +1,21 @@
-import { View } from "react";
-import { createAppContainer } from "react-navigation";
+import {} from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import Home from "../screens/Home";
 import Notifications from "../screens/Notifications";
 import Search from "../screens/Search";
 import Profile from "../screens/Profile";
 
-const TabNavigation = createBottomTabNavigator({
+export default createBottomTabNavigator({
   Home,
   Search,
   Add: {
     screen: Home,
     navigationOptions: {
-      tabBarOnPress: () => {
-        console.log("Add");
+      tabBarOnPress: ({ navigation }) => {
+        navigation.navigate("PhotoNavigation");
       }
     }
   },
   Notifications,
   Profile
 });
-
-export default createAppContainer(TabNavigation);
---
+++ screens/Photo/SelectPhoto.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const View = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.Text``;
+
+export default ({ navigation }) => (
+  <View>
+    <TouchableOpacity onPress={() => navigation.navigate("UploadPhoto")}>
+      <Text>Select Photo</Text>
+    </TouchableOpacity>
+  </View>
+);
