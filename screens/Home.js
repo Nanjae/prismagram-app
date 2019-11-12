ef0d5fd3296d165f2229a3fd91f7cccfb18684a0
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-11-12 12:21:38 +0900

    #14.0 Apollo Context

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,39 @@
 import React from "react";
+import { gql } from "apollo-boost";
 import styled from "styled-components";
 import Loader from "../components/Loader";
+import { useQuery } from "react-apollo-hooks";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 const View = styled.View`
   flex: 1;
@@ -10,8 +43,8 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const { loading, data } = useQuery(FEED_QUERY);
+  console.log(data);
+  return <View>{loading ? <Loader /> : <Text>Home</Text>}</View>;
+};
