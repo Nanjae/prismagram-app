efcd9a89533bfccc5d9a7f35642969761a8a6771
Nanjae <dngngn3045@gmail.com>
2019-11-15 07:04:32 +0900

    update UploadPhoto.js

--- screens/Photo/UploadPhoto.js
+++ screens/Photo/UploadPhoto.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { Image, StatusBar, Alert } from "react-native";
 import styled from "styled-components";
 import styles from "../../styles";
 import useInput from "../../hooks/useInput";
 import constants from "../../constants";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import FormData from "form-data";
 
 const View = styled.View``;
 
@@ -44,18 +46,37 @@ const Text = styled.Text`
 `;
 
 export default ({ navigation }) => {
-  const captionInput = useInput("");
-  const locationInput = useInput("");
   const [canUploadPhoto, setCanUploadPhoto] = useState(true);
+  const [fileUrl, setFileUrl] = useState("");
+  const photo = navigation.getParam("photo");
+  const captionInput = useInput("asfd");
+  const locationInput = useInput("");
   const handleSubmit = async () => {
     if (!canUploadPhoto) {
       return;
     }
+    setCanUploadPhoto(false);
+    if (captionInput.value === "") {
+      Alert.alert("문구를 입력해주세요.");
+    }
+    const formData = new FormData();
+    const name = photo.filename;
+    const [, type] = name.split(".");
+    formData.append("file", {
+      name,
+      type: type.toLowerCase(),
+      uri: photo.uri
+    });
+    console.log(formData);
     try {
-      setCanUploadPhoto(false);
-      if (captionInput.value === "") {
-        Alert.alert("문구를 입력해주세요.");
-      }
+      await axios.post(
+        "http://b2cbf171.ngrok.io/api/upload",
+        {
+          data: formData
+        },
+        { headers: { "Content-type": "multipart/form=data" } }
+      );
+      // console.log(data);
     } catch (e) {
       console.log(e);
       setCanUploadPhoto(true);
@@ -70,7 +91,7 @@ export default ({ navigation }) => {
         <Image
           style={{ width: 70, height: 70 }}
           source={{
-            uri: navigation.getParam("photo").uri
+            uri: photo.uri
           }}
         />
         <TextInputContainer>
